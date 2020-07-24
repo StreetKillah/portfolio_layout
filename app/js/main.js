a071acd88c13c0385635c597f85b0d4c785ddf44
Michael Prokopev <matroskin119@gmail.com>
2020-07-24 14:47:14 +0300

    немного адаптива

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.header__burger').click((e) =>{
-        $('.header__burger, nav, ul, .header__logo, .navbar').toggleClass('active');
+        $('.header__burger, nav, ul, li, .header__logo, .navbar').toggleClass('active');
         $('body').toggleClass('lock');
     })
 
